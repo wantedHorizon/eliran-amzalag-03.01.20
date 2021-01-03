8eacf042a4e4f3548fe8a1ca71724b75da5048bb
WantedHorizon <eliran.amzalag@gmail.com>
2021-01-03 21:28:19 +0200

    key

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 
 const key = process.env.REACT_APP_API_KEY;
-console.log(key);
 export default axios.create({
   baseURL: 'https://dataservice.accuweather.com',
   params: {
