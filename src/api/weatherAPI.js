c411cbdda2b9f06b55571f0d38e253d4ecc5f6f4
WantedHorizon <eliran.amzalag@gmail.com>
2021-01-03 21:03:54 +0200

    env

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 
-
+// wEAIRZASaw0JGxATP6zOeYfY8yd3pm3r
+const key = process.env.REACT_APP_API_KEY;
+console.log(key);
 export default axios.create({
   baseURL: 'https://dataservice.accuweather.com',
   params: {
-    apikey: "wEAIRZASaw0JGxATP6zOeYfY8yd3pm3r",
+    apikey: key,
   },
 });
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './store/reducers'
+require('dotenv').config()
+
 
 
 const store = createStore(reducers, applyMiddleware(thunk));
